c44412309652352d579aa1057fffa13dcdafc4cc
Ming Tam <ming@fotins.com>
2023-01-30 17:42:57 +0800

    Fix spelling scissor to scissors

--- src/main.rs
+++ src/main.rs
@@ -2,25 +2,25 @@
 enum Strategy {
     Rock,
     Paper,
-    Scissor,
+    Scissors,
 }
 
 fn judge(player1_strategy: Strategy, player2_strategy: Strategy) -> (i32, i32) {
     match (player1_strategy, player2_strategy) {
         (Strategy::Rock, Strategy::Rock) => (0, 0),
         (Strategy::Paper, Strategy::Paper) => (0, 0),
-        (Strategy::Scissor, Strategy::Scissor) => (0, 0),
+        (Strategy::Scissors, Strategy::Scissors) => (0, 0),
         (Strategy::Rock, Strategy::Paper) => (-1, 1),
-        (Strategy::Paper, Strategy::Scissor) => (-1, 1),
-        (Strategy::Scissor, Strategy::Rock) => (-1, 1),
+        (Strategy::Paper, Strategy::Scissors) => (-1, 1),
+        (Strategy::Scissors, Strategy::Rock) => (-1, 1),
         (Strategy::Paper, Strategy::Rock) => (1, -1),
-        (Strategy::Scissor, Strategy::Paper) => (1, -1),
-        (Strategy::Rock, Strategy::Scissor) => (1, -1),
+        (Strategy::Scissors, Strategy::Paper) => (1, -1),
+        (Strategy::Rock, Strategy::Scissors) => (1, -1),
     }
 }
 
 fn main() {
     println!("Hello, world!");
     println!("{:?}", judge(Strategy::Rock, Strategy::Paper));
-    println!("{:?}", judge(Strategy::Scissor, Strategy::Paper));
+    println!("{:?}", judge(Strategy::Scissors, Strategy::Paper));
 }
