18d8a8c8f96ab1be70acad38c121dd40a1a82fcf
Ming Tam <ming@fotins.com>
2023-01-31 14:18:08 +0800

    Made a player
    Who randomly choose rock, paper, or scissors.

--- src/main.rs
+++ src/main.rs
@@ -1,4 +1,6 @@
+use rand::{thread_rng, Rng, rngs::ThreadRng};
 
+#[derive(Debug)]
 enum Strategy {
     Rock,
     Paper,
@@ -19,8 +21,49 @@ fn judge(player1_strategy: Strategy, player2_strategy: Strategy) -> (i32, i32) {
     }
 }
 
+#[derive(Debug)]
+struct Player {
+    rock: u32,
+    paper: u32,
+    scissors: u32,
+    score: i32,
+}
+impl Player {
+    pub fn new() -> Player {
+        Player {
+            rock: 100000,
+            paper: 100000,
+            scissors: 100000,
+            score: 0,
+        }
+    }
+
+    pub fn decide(self, rng: &mut ThreadRng) -> Strategy {
+        let mut choice: u32 = rng.gen_range(0..(self.rock + self.paper + self.scissors));
+
+        if choice < self.rock {
+            return Strategy::Rock;
+        }
+
+        choice -= self.rock;
+
+        if choice < self.paper {
+            return Strategy::Paper;
+        }
+
+        Strategy::Scissors
+    }
+}
+
 fn main() {
     println!("Hello, world!");
     println!("{:?}", judge(Strategy::Rock, Strategy::Paper));
     println!("{:?}", judge(Strategy::Scissors, Strategy::Paper));
+
+    let mut rng = thread_rng();
+    rng.gen_range(0..10);
+
+    let player_1 = Player::new();
+    println!("Player 1: {:?}", player_1);
+    println!("Player 1 decide: {:?}", player_1.decide(&mut rng));
 }
